e6398c30ccd3d55f3c8146b5066a2401870a7af7
Niko <niko.barber@gmail.com>
2025-06-02 22:21:58 -0300

    Registros file add

---
+++ src/main/java/org/example/Registros.java
@@ -0,0 +1,31 @@
+package Kopius.Academy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Registros {
+    String chromeDriverPath = "/Workspace/chromedriver/chromedriver.exe";
+    String pathTorneoHTML = "file:///C:/Workspace/KopiusAcademy/src/main/resources/TorneoHTML/";
+    //String webRegistro = "file:///C:/Workspace/KopiusAcademy/src/main/resources/TorneoHTML/registro.html";
+        public static void main(String[] args) {
+            System.out.println("Todo listo para la ejecucion.");
+            new Registros();
+        }
+
+public Registros(){
+    System.out.println("Comienza el Test 1");
+    System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+    WebDriver driver = new ChromeDriver();
+    driver.get(pathTorneoHTML+"registro.html");
+
+    String currentUrl = driver.getCurrentUrl();
+    System.out.println("Abrimos Pagina: " + currentUrl);
+
+}
+}
